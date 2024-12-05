e85231b0882b94075208bcf25be5bc96bf226007
cat0527 <caterin.mendez0527@gmail.com>
2024-12-05 18:48:35 -0300

    primerCommit

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import {createStore} from "vuex";
+
+export default createStore({
+  state: {}, // variables de estado
+  mutations: {}, // cambio de valor de variables de estado
+  actions: {}, // comunicaciones externas con una API
+  getters: {}, 
+  modules: {},
+})
+src/store/index.js
